cd9023ce5f8c57ff3507a33d550c2e1dd7d61bc5
Alexey Savchenko <alexey.savchenko.abweb@gmail.com>
2017-06-07 15:22:18 +0300

    Переделал все нафиг! Осталось только сделать рендер новых полей

--- client/main.js
+++ client/main.js
@@ -2,65 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './components/App.jsx';
 
-class Fields extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            title: 'title'
-        };
-        this.handleChangeTitle = this.handleChangeTitle.bind(this);
-    }
-
-    handleChangeTitle(event) {
-        this.setState({title: event.target.value});
-    }
-    render() {
-        return (
-            <div className="row_fields_form">
-                <input className="title" type="text" placeholder="Название" value={this.state.title} onChange={this.handleChangeTitle} />
-                <input className="description" type="text" placeholder="Описание" />
-            </div>
-        )
-    }
-}
-
 
 class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            amount: 1
+            amount: 0,
+            ['title'+ this.amount ? 'title0':'']: '',
+            ['description'+ this.amount ? 'description0':'']: ''
         };
         this.handleAddField = this.handleAddField.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.fields = <Fields/>;
-        this.list_fields = [<Fields/>];
+    }
+
+    handleFieldChange(fieldName,e) {
+        this.setState(({[''+fieldName]: e.target.value}))
     }
 
     handleAddField(e) {
-        this.setState({
-            amount: ++this.state.amount
-        });
-        this.list_fields.push(this.fields);
         e.preventDefault();
+        ++this.state.amount;
+        this.setState({
+            [['title'+this.state.amount]]: e.target.value,
+            [['description'+this.state.amount]]: e.target.value
+        });
     }
 
-    handleSubmit(event) {
-
-        const fields_title = this.list_fields.map((field, i) =>
-            console.log(field.props.title));
-
-        alert('An essay was submitted: ' + fields_title);
-        event.preventDefault();
+    handleSubmit(e) {
+        e.preventDefault();
+        for(let field in this.state){
+            console.log(field + ' : ' + this.state[field]);
+        }
     }
 
     render() {
-        const list_fields = this.list_fields.map((field, i) =>
-            <li key={i}>{ field } </li>
-        );
         return(
             <form className="form" onSubmit={this.handleSubmit}>
-                <ol>{list_fields}</ol>
+                <ol>
+                    <div className="row_fields_form">
+                        <input className="title" type="text" name="title" placeholder="Название" value={this.state.title} onChange={this.handleFieldChange.bind(this, 'title'+this.state.amount)} />
+                        <input className="description" type="text" name="description" placeholder="Описание" onChange={this.handleFieldChange.bind(this, 'description'+this.state.amount)} />
+                    </div>
+                </ol>
                 <input type="submit" value="Создать" />
                 <button onClick={this.handleAddField}>+</button>
             </form>
@@ -80,7 +63,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Form  />
+                <Form />
             </div>
         );
     }
