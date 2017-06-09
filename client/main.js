a502b54dacbd7a3bdbfcfbc850221ab5661d490d
Alexey Savchenko <alexey.savchenko.abweb@gmail.com>
2017-06-09 17:24:43 +0300

    Таймер и Notes

--- client/main.js
+++ client/main.js
@@ -1,102 +1,154 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App.jsx';
+import Timer from './components/Timer.jsx';
 
+class Note extends React.Component {
+    render(){
+        let style = { backgroundColor: this.props.color };
+        return (
+            <div className="note" style={style}>
+                <span className="delete-note" onClick={this.props.onDelete}> × </span>
+                {this.props.children}
+            </div>
+        );
+    }
+}
 
-class Form extends React.Component {
-    constructor(props) {
+class NoteEditor extends React.Component {
+    constructor(props){
         super(props);
         this.state = {
-            amount: 1,
-            ['title'+ this.amount ? 'title0':'']: '',
-            ['description'+ this.amount ? 'description0':'']: ''
+            text: ''
         };
-        this.handleAddField = this.handleAddField.bind(this);
-        this.handleDelField = this.handleDelField.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.count = 0;
-        this.fields_group = [];
     }
 
-    handleFieldChange(fieldName,e) {
-        this.setState(({[''+fieldName]: e.target.value}))
-    }
-
-    handleAddField(e) {
-        e.preventDefault();
-        ++this.state.amount;
+    handleTextChange(event) {
         this.setState({
-            [['title'+this.state.amount]]: e.target.value,
-            [['description'+this.state.amount]]: e.target.value
+            text: event.target.value
         });
     }
 
-    handleDelField(event) {
-        event.preventDefault();
-        console.log(event);
-        if(this.state.amount > 1){
-            delete this.state['title'+this.state.amount];
-            delete this.state['description'+this.state.amount];
-            this.setState({
-                amount: --this.state.amount
-            });
-            this.fields_group.pop();
-            --this.count;
-        }
-        const formData = new Form(document.querySelector('div'));
-        console.log(formData)
-    }
+    handleNoteAdd() {
+    let newNote = {
+        text: this.state.text,
+        color: 'yellow',
+        id: Date.now()
+    };
 
-    handleSubmit(e) {
-        e.preventDefault();
-        for(let field in this.state){
-            console.log(field + ' : ' + this.state[field]);
-        }
-    }
-
+    this.props.onNoteAdd(newNote);
+    this.setState({ text: '' });
+}
     render() {
-        while(this.count < this.state.amount){
-
-                this.fields_group.push(
-                    <div className="row_fields_form">
-                        <input className="title" type="text" name={'title'+this.state.amount} placeholder="Название" onChange={this.handleFieldChange.bind(this, 'title'+this.state.amount)} />
-                        <input className="description" type="text" name={'description'+this.state.amount} placeholder="Описание" onChange={this.handleFieldChange.bind(this, 'description'+this.state.amount)} />
-                        <button name={'delete_btn'+this.state.amount} onClick={this.handleDelField}>-</button>
-                    </div>
-                );
-                this.count++;
-        }
-
-        const list_fields = this.fields_group.map((field, i) =>
-            <li key={i}> { field } </li>
+        return (
+            <div className="note-editor">
+                    <textarea
+                        placeholder="Enter your note here..."
+                        rows={5}
+                        className="textarea"
+                        value={this.state.text}
+                        onChange={this.handleTextChange.bind(this)}
+                    />
+                <button className="add-button" onClick={this.handleNoteAdd.bind(this)}>Add</button>
+            </div>
         );
+    }
+}
 
-        console.log('render');
-        return(
-            <form className="form" onSubmit={this.handleSubmit}>
-                <ol>
-                    {list_fields}
-                </ol>
-                <input type="submit" value="Создать" />
-                <button onClick={this.handleAddField}>+</button>
-                <button onClick={this.handleDelField}>-</button>
-            </form>
+class NotesGrid extends React.Component {
+
+    componentDidMount() {
+    let grid = this.refs.grid;
+    this.msnry = new Masonry( grid, {
+        itemSelector: '.note',
+        columnWidth: 200,
+        gutter: 10,
+        isFitWidth: true
+    });
+}
+
+    componentDidUpdate(prevProps) {
+    if (this.props.notes.length !== prevProps.notes.length) {
+        this.msnry.reloadItems();
+        this.msnry.layout();
+    }
+}
+
+    render(){
+        let onNoteDelete = this.props.onNoteDelete;
+        return (
+            <div className="notes-grid" ref="grid">
+                {
+                    this.props.notes.map(function(note){
+                        return (
+                            <Note
+                                key={note.id}
+                                onDelete={onNoteDelete.bind(null, note)}
+                                color={note.color}>
+                                {note.text}
+                            </Note>
+                        );
+                    })
+                }
+            </div>
         )
     }
 }
 
-class App extends React.Component {
+class NotesApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            notes: []
         };
     }
 
+    componentDidMount(){
+        let localNotes = JSON.parse(localStorage.getItem('notes'));
+        if (localNotes) {
+            this.setState({ notes: localNotes });
+        }
+    }
+
+    componentDidUpdate() {
+    this._updateLocalStorage();
+}
+
+    handleNoteAdd(newNote){
+        let newNotes = this.state.notes.slice();
+        newNotes.unshift(newNote);
+        this.setState({ notes: newNotes });
+    }
+
+    handleNoteDelete(note) {
+    let noteId = note.id;
+    let newNotes = this.state.notes.filter(function(note) {
+        return note.id !== noteId;
+    });
+    this.setState({ notes: newNotes });
+}
+
+    render() {
+        return (
+            <div className="notes-app">
+                <h2 className="app-header">NotesApp</h2>
+                <NoteEditor onNoteAdd={this.handleNoteAdd.bind(this)} />
+                <NotesGrid notes={this.state.notes} onNoteDelete={this.handleNoteDelete.bind(this)} />
+            </div>
+        );
+    }
+
+    _updateLocalStorage() {
+        let notes = JSON.stringify(this.state.notes);
+        localStorage.setItem('notes', notes);
+    }
+}
+
+class App extends React.Component {
     render() {
         return (
             <div>
-                <Form />
+                <NotesApp/>
+                <Timer/>
             </div>
         );
     }
